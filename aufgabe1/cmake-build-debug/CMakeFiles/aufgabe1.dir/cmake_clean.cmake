9017cd99588c68f7b12af6101519ff05f61f3120
Exokay88 <tobias.seemann@hotmail.com>
2017-05-10 16:59:36 +0200

    Client Server Socket

---
+++ aufgabe1/cmake-build-debug/CMakeFiles/aufgabe1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aufgabe1.dir/main.c.o"
+  "aufgabe1.pdb"
+  "aufgabe1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aufgabe1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
